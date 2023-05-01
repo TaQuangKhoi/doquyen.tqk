b2efbb7562041fec7ed0c970782b450b49a9ca1e
Tạ Quang Khôi <khoi0941@gmail.com>
2023-05-01 11:28:40 +0700

    🔧 chore(.gitignore): bỏ dấu * ở cuối tên file .env 🚀 feat(background.ts): thêm tích hợp Firebase và lưu lịch sử truy cập 🚀 feat(package.json): thay đổi tên extension thành heynight-abc-extension và thêm dependency firebase Bỏ dấu * ở cuối tên file .env giúp gitignore chỉ bỏ qua file .env cụ thể, không bỏ qua tất cả các file có tên bắt đầu bằng .env. Tích hợp Firebase giúp lưu lịch sử truy cập vào Firestore. Thay đổi tên extension và thêm dependency firebase giúp cải thiện tính nhất quán trong việc đặt tên

--- heyday-abc-extension/background.ts
+++ heyday-abc-extension/background.ts
@@ -1,6 +1,72 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
 console.log(
   "Live now; make now always the most precious time. Now will never come again."
 )
+
+const firebaseConfig = {
+  apiKey: process.env.PLASMO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "heyday-abc.firebaseapp.com",
+  projectId: "heyday-abc",
+  storageBucket: "heyday-abc.appspot.com",
+  messagingSenderId: "767191939509",
+  appId: "1:767191939509:web:6ead3ed554ed12820fc1fc",
+  measurementId: "G-V4D9SEPHF4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const historyRef = collection(db, "history");
+
+async function TestFirebase() {
+
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+// TestFirebase().then(() => {
+//   console.log("TestFirebase done")
+// })
+
+async function saveHistoryItem(historyItem: chrome.history.HistoryItem) {
+  try {
+    const docRef = await addDoc(historyRef, {
+      lastVisitTime: historyItem.lastVisitTime,
+      title: historyItem.title,
+      typedCount: historyItem.typedCount,
+      url: historyItem.url,
+      visitCount: historyItem.visitCount
+    });
+    console.log("History written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function getHistoryItems() {
+  return getDocs(collection(db, "history"))
+}
+
+// getHistoryItems().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     console.log(`${doc.id} => ${doc.data()}`);
+//   });
+// })
 
 chrome.history.search(
   {
@@ -9,6 +75,11 @@ chrome.history.search(
     maxResults: 1000000
   },
   (historyItems) => {
-    console.log(historyItems)
+    // historyItems.map((historyItem) => {
+    //   console.log(historyItem)
+    //   // saveHistoryItem(historyItem).then(() => {
+    //   //   console.log("saveHistoryItem done")
+    //   // })
+    // })
   }
 )
